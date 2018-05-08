474f04feb84cff1c4e319d759c304cad5d079df0
fedor <fedor.korotkov@gmail.com>
2018-05-08 09:43:32 -0400

    Flutter 0.4.0

--- build_docker.sh
+++ build_docker.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker build --tag cirrusci/flutter:base base
-docker build --tag cirrusci/flutter:0.2.10 --tag cirrusci/flutter:latest 0.2.10
+docker build --tag cirrusci/flutter:0.4.0 --tag cirrusci/flutter:latest 0.4.0
