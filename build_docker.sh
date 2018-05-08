ffab446f35fb7535be4b51d2a15c86f608a1ba09
fedor <fedor.korotkov@gmail.com>
2018-05-08 09:56:38 -0400

    pull base to make sure it's not invalidated

--- build_docker.sh
+++ build_docker.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+# pull to make sure we are not rebuilding for nothing
+docker pull cirrusci/flutter:base
+
 docker build --tag cirrusci/flutter:base base
 docker build --tag cirrusci/flutter:0.4.0 --tag cirrusci/flutter:latest 0.4.0
