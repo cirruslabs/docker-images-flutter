f70f0c7818c65f2f62ff582bd866850224c99191
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-04-27 14:00:42 +0700

    expose different versions and channels
    See more at https://flutter.dev/docs/development/tools/sdk/releases

--- build_docker.sh
+++ build_docker.sh
@@ -7,6 +7,11 @@ docker pull cirrusci/flutter:base
 
 docker build --tag cirrusci/flutter:base base
 docker build --tag cirrusci/flutter:$FLUTTER_VERSION \
-             --tag cirrusci/flutter:latest \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
+
+if [ $FLUTTER_VERSION == "stable" ]
+then
+  # `stable` channel will be our `latest`
+  docker tag cirrusci/flutter:$FLUTTER_VERSION cirrusci/flutter:latest
+fi
