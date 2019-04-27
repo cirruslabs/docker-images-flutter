b3309877c031cb649fcc20c360c874c0c0a67d41
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-04-27 21:41:41 +0700

    resolve feedback

--- build_docker.sh
+++ build_docker.sh
@@ -7,11 +7,6 @@ docker pull cirrusci/flutter:base
 
 docker build --tag cirrusci/flutter:base base
 docker build --tag cirrusci/flutter:$FLUTTER_VERSION \
+             --tag cirrusci/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
-
-if [ $FLUTTER_VERSION == "stable" ]
-then
-  # `stable` channel will be our `latest`
-  docker tag cirrusci/flutter:$FLUTTER_VERSION cirrusci/flutter:latest
-fi
