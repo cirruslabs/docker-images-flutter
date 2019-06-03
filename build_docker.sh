5b8b57f2cc658c96f2b580108a6a07563da8a138
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-06-03 08:26:31 -0400

    Flutter Web Images (#19)
    Fixes #17

--- build_docker.sh
+++ build_docker.sh
@@ -5,8 +5,15 @@ set -e
 # pull to make sure we are not rebuilding for nothing
 docker pull cirrusci/flutter:base
 
-docker build --tag cirrusci/flutter:base base
-docker build --tag cirrusci/flutter:$FLUTTER_VERSION \
+docker build --cache-from cirrusci/flutter:base \
+             --tag cirrusci/flutter:base base
+docker build --cache-from cirrusci/flutter:$FLUTTER_VERSION \
+             --tag cirrusci/flutter:$FLUTTER_VERSION \
              --tag cirrusci/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
+docker build --cache-from cirrusci/flutter:$FLUTTER_VERSION-web \
+             --tag cirrusci/flutter:$FLUTTER_VERSION-web \
+             --tag cirrusci/flutter:$DOCKER_TAG-web \
+             --build-arg flutter_version=$FLUTTER_VERSION \
+             web
