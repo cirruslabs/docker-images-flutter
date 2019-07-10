b60d3fac429b6fe813c1a6c6d1fef007a97c0b01
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-07-10 09:31:47 -0400

    Uograde Flutter
    And support flutter version with `+`. It was breaking Docker tagging since tags don't support `+`.
    
    Fixes #20
    Fixes #22

--- build_docker.sh
+++ build_docker.sh
@@ -7,13 +7,13 @@ docker pull cirrusci/flutter:base
 
 docker build --cache-from cirrusci/flutter:base \
              --tag cirrusci/flutter:base base
-docker build --cache-from cirrusci/flutter:$FLUTTER_VERSION \
-             --tag cirrusci/flutter:$FLUTTER_VERSION \
+docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-} \
+             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-} \
              --tag cirrusci/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
-docker build --cache-from cirrusci/flutter:$FLUTTER_VERSION-web \
-             --tag cirrusci/flutter:$FLUTTER_VERSION-web \
+docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
+             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
              --tag cirrusci/flutter:$DOCKER_TAG-web \
-             --build-arg flutter_version=$FLUTTER_VERSION \
+             --build-arg flutter_tag=${FLUTTER_VERSION/+/-} \
              web
