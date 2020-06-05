9b8c72cd28de3d8e38aae67a8ba7e4383579bde1
fkorotkov <fedor.korotkov@gmail.com>
2020-06-05 13:06:58 -0400

    Disabled web builds
    Related to #66

--- build_docker.sh
+++ build_docker.sh
@@ -7,8 +7,8 @@ docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-} \
              --tag cirrusci/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
-docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
-             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
-             --tag cirrusci/flutter:$DOCKER_TAG-web \
-             --build-arg flutter_tag=${FLUTTER_VERSION/+/-} \
-             web
+#docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
+#             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
+#             --tag cirrusci/flutter:$DOCKER_TAG-web \
+#             --build-arg flutter_tag=${FLUTTER_VERSION/+/-} \
+#             web
--- push_docker.sh
+++ push_docker.sh
@@ -11,7 +11,7 @@ docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 docker push cirrusci/flutter:${FLUTTER_VERSION/+/-}
 docker push cirrusci/flutter:$DOCKER_TAG
-docker push cirrusci/flutter:${FLUTTER_VERSION/+/-}-web
-docker push cirrusci/flutter:$DOCKER_TAG-web
+#docker push cirrusci/flutter:${FLUTTER_VERSION/+/-}-web
+#docker push cirrusci/flutter:$DOCKER_TAG-web
 
 curl -X POST https://hooks.microbadger.com/images/cirrusci/flutter/jvUQ_07dNfmCwsULt6N9qYdgsNg=
