db6846598e7677999be050fca6a74615e21e9685
fedor <fedor@cirruslabs.org>
2021-05-09 22:09:28 -0400

    Build ARM arch
    Fixes https://github.com/cirruslabs/docker-images-flutter/issues/150

--- build_docker.sh
+++ build_docker.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-} \
+docker buildx build --platform linux/amd64,linux/arm64 \
+             --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-} \
              --tag cirrusci/flutter:${FLUTTER_VERSION/+/-} \
              --tag cirrusci/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
              sdk
-#docker build --cache-from cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
-#             --tag cirrusci/flutter:${FLUTTER_VERSION/+/-}-web \
-#             --tag cirrusci/flutter:$DOCKER_TAG-web \
-#             --build-arg flutter_tag=${FLUTTER_VERSION/+/-} \
-#             web
