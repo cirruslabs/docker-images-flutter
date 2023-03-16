2b53fd6cf1e34eb4f53fb73a49aad93d62337c4a
Fedor Korotkov <fedor@cirruslabs.org>
2023-03-16 12:55:49 -0400

    Switch to GitHub's Container registry (#260)
    Because Docker is closing free tier for Docker Hub

--- push_docker.sh
+++ push_docker.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker history cirrusci/flutter:${FLUTTER_VERSION/+/-}
-docker history cirrusci/flutter:$DOCKER_TAG
+docker history ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION/+/-}
+docker history ghcr.io/cirruslabs/flutter:$DOCKER_TAG
 
 if [ "$CIRRUS_BRANCH" != "master" ]
 then
@@ -12,7 +12,7 @@ fi
 
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
-docker push cirrusci/flutter:${FLUTTER_VERSION/+/-}
-docker push cirrusci/flutter:$DOCKER_TAG
-#docker push cirrusci/flutter:${FLUTTER_VERSION/+/-}-web
-#docker push cirrusci/flutter:$DOCKER_TAG-web
+docker push ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION/+/-}
+docker push ghcr.io/cirruslabs/flutter:$DOCKER_TAG
+#docker push ghcr.io/cirruslabs/flutter:${FLUTTER_VERSION/+/-}-web
+#docker push ghcr.io/cirruslabs/flutter:$DOCKER_TAG-web
